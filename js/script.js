9b21d05048298be3739fb9d43bcbca36d978deac
mtrupiano <markt6749@gmail.com>
2020-12-10 11:54:43 -0800

    simplify logic; increase probability of numbers appearing in password

--- js/script.js
+++ js/script.js
@@ -11,34 +11,21 @@ $(function() {
             // show error near size entry field
             return;
         }
-        
-
-        // Prompt user to enter password length (between 8 and 128 inclusive)
-        // while (numChars < 8 || numChars > 128) {
-        //     numChars = prompt("Enter number of characters in your password (min 8, max 128)");
-        // }
 
         var charTypes = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ",
                          "abcdefghijklmnopqrstuvwxyz",
-                         "0123456789",
+                         "01234567890123456789",    // Repeating the digits increases chances for a
+                                                    // number to appear in the resulting password
                          " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"]
-        
-        var includedChars = [false, // Include upper-case letters?
-                             false, // Include lower-case letters?
-                             false, // Include numeric characters?
-                             false] // Include special characters?
-        
-        // Prompt user to select character types (user must select at least 1 character type)                     
-        while (!includedChars.includes(true)) {
-        
-            includedChars[0] = $('#include-upper').is(':checked'); // confirm("Include upper-case letters?");
-            includedChars[1] = $('#include-lower').is(':checked'); // confirm("Include lower-case letters?");
-            includedChars[2] = $('#include-numeric').is(':checked'); // confirm("Include numeric characters?");
-            includedChars[3] = $('#include-special').is(':checked'); // confirm("Include special characters?");
-        
-            if (!includedChars.includes(true)) {
-                alert("You must select at least one character type.");
-            }
+
+        var includedChars = [$('#include-upper').is(':checked'),
+                             $('#include-lower').is(':checked'),
+                             $('#include-numeric').is(':checked'),
+                             $('#include-special').is(':checked')]
+    
+        if (!includedChars.includes(true)) {
+            alert("You must select at least one character type.");
+            return;
         }
         
         var charSpace = "";
